e0a7ef471827f87f2ca26819e37355b4e7eca3f3
Brad Campbell <bradjc5@gmail.com>
2015-12-25 01:01:19 -0500

    update advertise parse, better readme
    more defined functions in parse.js

--- software/advertisement-parse/README.md
+++ software/advertisement-parse/README.md
@@ -1,5 +1,18 @@
-Advertisement Parser
+Ble Gateway
 ====================
+
+This is designed to be a modular gateway for BLE devices.
+Its core principle is allowing devices to specify how to parse the data
+they provide and what to do with that data.
+It does this by allowing devices to specify their own JavaScript functions
+that now how to take the data encoded in their advertisements and services
+and format it into a JavaScript object. The gateway further allows devices
+to provide JavaScript functions that know what to do with the parsed data.
+
+
+
+Quick Overview
+--------------
 
 This script:
 
@@ -8,16 +21,115 @@ This script:
 2. Upon finding one, pulls out the URL and tries to fetch `<URL>/parse.js`.
 
 3. If that exists, it uses the resulting JS file to parse other advertisements
-the device sends in the future. The parsed advertisement comes in the form
-of an object and is printed to stdout.
+the device sends in the future.
+
+
+To use this gateway with your BLE device
+------------------------------------
+
+1. Configure your device to advertise two different advertisements.
+One should be an Eddystone URL packet. The URL should point to a webserver
+path where you can host the needed JavaScript code. For example, the Eddystone
+URL should be a shortened version of something like:
+
+        https://github.com/org/project/device/
+
+    The second advertisement can be completely device specific. It can contain
+    data or not. The second advertisement is what the gateway will use
+    to parse data from the device.
+
+    If you wish, you can advertise additional custom advertisements. Each will
+    be passed to the same parse function, however.
+
+2. Create a `parse.js` file and host it in the directory pointed to by the
+Eddystone URL. For example:
+
+        https://github.com/org/project/device/parse.js
+
+    See below for how to create the `parse.js` file.
 
 
 
+`parse.js`
+----------
 
-Usage
+To create a `parse.js` file you simply implement the functions you wish to
+support. The basic template looks like:
+
+```js
+
+// This function is called by the gateway when a non-Eddystone advertisement
+// is received. The function should take the noble formatted advertisement,
+// parse it into a JavaScript object, and call done() with that object.
+var parseAdvertisement = function (advertisement, done) {};
+
+// This function allows you to return an object with data collected from
+// reading device services and characteristics. When this function is called,
+// the gateway will pass in a noble peripheral object that is already connected.
+// This function must read and services and characteristics that it wishes to
+// and call done() with a JavaScript object.
+var parseServices = function (peripheral, done) {};
+
+// This function will be called with the parsed advertisement data and allows
+// you to do something application specific with it.
+var publishAdvertisement = function (adv_obj) {};
+
+// This function allows you to publish formatted data from services.
+var publishServiceData = function (data_obj) {};
+
+// Only include here the functions you support. The gateway will only
+// call the functions that are exported.
+module.exports = {
+    parseAdvertisement: parseAdvertisement,
+    parseServices: parseServices,
+    publishAdvertisement: publishAdvertisement,
+    publishServiceData: publishServiceData,
+};
+```
+
+
+Extending the Gateway
+---------------------
+
+This gateway can run as a standalone application or as a module inside
+of another tool.
+
+
+```js
+
+var BleGateway = require('BleGateway');
+
+// Receive formatted advertisement data objects.
+// adv_obj.id will be the peripheral id that it was captured from.
+BleGateway.on('advertisements', function (adv_obj) {
+	...
+});
+
+// Receive formatted objects from reading device services.
+// data_obj.id will be the peripheral id that it was captured from.
+BleGateway.on('data', function (data_obj) {
+	...
+});
+
+// Get everything going.
+BleGateway.start();
+```
+
+
+Gateway Usage
 -----
 
 ```
 npm install
 ./advertisement-parse.js
 ```
+
+Questions
+---------
+
+- How to let devices trigger common parsing functions (like Cloudcomm)?
+Likely that a device will want to be able to use a common gateway function.
+
+- Is there anyway to make downloading and running arbitrary JavaScript safe?
+
+
--- software/advertisement-parse/advertisement-parse.js
+++ software/advertisement-parse/advertisement-parse.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+var events = require('events');
 var url = require('url');
+var util = require('util');
 
 var noble = require('noble');
 var request = require('request');
@@ -8,17 +10,43 @@ var EddystoneBeaconScanner = require('eddystone-beacon-scanner');
 var urlExpander = require('expand-url');
 var _ = require('lodash');
 
+var argv = require('yargs')
+    .help('h')
+    .alias('h', 'help')
+    .option('no-parse-advertisements', {
+        describe: 'this gateway should not parse advertisements',
+        boolean: true,
+    })
+    .option('no-parse-services', {
+        describe: 'this gateway should not connect and parse services',
+        boolean: true
+    })
+    .option('no-publish', {
+        describe: 'this gateway should not publish parsed data',
+        boolean: true
+    })
+    .argv;
+
 
 var FILENAME_PARSE = 'parse.js'
-var FILENAME_META = 'meta.json'
+// var FILENAME_META = 'meta.json'
 
 
 // Store know things about each BLE device
-var beaconid_to_data = {};
+// var beaconid_to_data = {};
 
 
+var BleGateway = function () {
+    this._device_to_data = {};
 
-function start () {
+    noble.on('discover', this.on_discover.bind(this));
+    EddystoneBeaconScanner.on('found', this.on_beacon.bind(this));
+};
+
+util.inherits(BleGateway, events.EventEmitter);
+
+// Call .start() to run the gateway functionality
+BleGateway.prototype.start = function () {
   var startScanningOnPowerOn = function() {
     if (noble.state === 'poweredOn') {
       noble.startScanning([], true);
@@ -30,46 +58,106 @@ function start () {
   startScanningOnPowerOn();
 };
 
-noble.on('discover', function (peripheral) {
+// noble.on('discover', function (peripheral) {
+BleGateway.prototype.on_discover = function (peripheral) {
 
     // Don't want the Eddystone beacons at the moment.
     if (!EddystoneBeaconScanner.isBeacon(peripheral)) {
 
         // We have seen an eddystone packet from the same address
-        if (peripheral.id in beaconid_to_data) {
+        if (peripheral.id in this._device_to_data) {
             console.log('Recognized device: ' + peripheral.id);
 
-            var device = beaconid_to_data[peripheral.id];
+            var device = this._device_to_data[peripheral.id];
 
-            // Check if we have some way to parse the advertisement
-            if (device.parser && device.parser.parse_advertisement) {
-                var out = device.parser.parse_advertisement(peripheral.advertisement);
-                out.id = peripheral.id;
-                console.log(out);
+            // Unless told not to, we parse advertisements
+            if (!argv.noParseAdvertisements) {
 
-                // Check if we have a meta file that describes what we should
-                // do with the data once we get it.
-                if (device.meta && device.meta.gateway &&
-                    device.meta.gateway.advertisement &&
-                    device.meta.gateway.advertisement.actions) {
-                    var actions = device.meta.gateway.advertisement.actions;
-                    _.forEach(actions, function (action) {
-                        if (action.action == 'POST') {
-                            console.log('POST TO ' + action.url);
+                // Check if we have some way to parse the advertisement
+                if (device.parser && device.parser.parseAdvertisement) {
 
-                            // TODO, make this do something...
+                    var parse_advertisement_done = function (adv_obj) {
+                        adv_obj.id = peripheral.id;
+                        console.log(adv_obj);
 
+                        // We broadcast on "advertisement"
+                        this.emit('advertisement', adv_obj);
+
+                        // Now check if the device wants to do something
+                        // with the parsed advertisement.
+                        if (!argv.noPublish && device.parser.publishAdvertisement) {
+                            device.parser.publishAdvertisement(adv_obj);
+                        }
+                    };
+
+                    // Call the device specific advertisement parse function.
+                    // Give it the done callback.
+                    device.parser.parseAdvertisement(peripheral.advertisement, parse_advertisement_done);
+                    
+
+                    // // Check if we have a meta file that describes what we should
+                    // // do with the data once we get it.
+                    // if (device.meta && device.meta.gateway &&
+                    //     device.meta.gateway.advertisement &&
+                    //     device.meta.gateway.advertisement.actions) {
+                    //     var actions = device.meta.gateway.advertisement.actions;
+                    //     _.forEach(actions, function (action) {
+                    //         if (action.action == 'POST') {
+                    //             console.log('POST TO ' + action.url);
+
+                    //             // TODO, make this do something...
+
+                    //         }
+                    //     });
+
+                    // }
+                }
+            }
+
+            // Unless told not to, we see if this device wants us to connect
+            if (!argv.noParseServices) {
+
+                var parse_services_done = function (data_obj) {
+                    data_obj.id = peripheral.id;
+                    console.log(data_obj);
+
+                    // After device-specific code is done, disconnect and handle
+                    // returned object.
+                    peripheral.disconnect(function (disconnect_error) {
+                        if (!disconnect_error) {
+                            // Broadcast this on "data"
+                            this.emit('data', data_obj);
+
+                            // Now check if the device wants to do something
+                            // with the parsed service data.
+                            if (!argv.noPublish && device.parser.publishServiceData) {
+                                device.parser.publishServiceData(data_obj);
+                            }
                         }
                     });
+                }
 
+                // Check if we have some code to connect
+                if (device.parser && device.parser.parseServices) {
+                    // Use noble to connect to the BLE device
+                    peripheral.connect(function (connect_error) {
+                        if (!connect_error) {
+                            // After a successful connection, let the
+                            // device specific code read services and whatnot.
+                            device.parser.parseServices(peripheral, parse_services_done);
+                        }
+                    });
                 }
             }
 
         }
+    } else {
+        console.log('Not interested: ' + peripheral.id);
     }
-});
+};
 
-function require_from_string (src, filename) {
+// function require_from_string (src, filename) {
+BleGateway.prototype.require_from_string = function (src, filename) {
     var m = new module.constructor();
     m.paths = module.paths;
     m._compile(src, filename);
@@ -79,7 +167,8 @@ function require_from_string (src, filename) {
 // We want just the base URL.
 // So, something like "https://a.com/folder/page.html?q=1#here"
 // should turn in to "https://a.com/folder/"
-function get_base_url (full_url) {
+// function get_base_url (full_url) {
+BleGateway.prototype.get_base_url = function (full_url) {
     var parsed_url = url.parse(full_url);
     parsed_url.query = '';
     parsed_url.hash = '';
@@ -96,7 +185,8 @@ function get_base_url (full_url) {
 }
 
 // Callback when an eddystone beacon is found.
-EddystoneBeaconScanner.on('found', function (beacon) {
+// EddystoneBeaconScanner.on('found', function (beacon) {
+BleGateway.prototype.on_beacon = function (beacon) {
     if (beacon.type == 'url') {
         console.log('Found eddystone: ' + beacon.id + ' ' + beacon.url);
 
@@ -104,46 +194,53 @@ EddystoneBeaconScanner.on('found', function (beacon) {
         urlExpander.expand(beacon.url, function (err, full_url) {
             if (!err) {
                 // Create space if this is a new beacon
-                if (!(beacon.id in beaconid_to_data)) {
-                    beaconid_to_data[beacon.id] = {};
+                if (!(beacon.id in this._device_to_data)) {
+                    this._device_to_data[beacon.id] = {};
                 }
 
                 // Get only the base (not index.html, for instance)
-                var base_url = get_base_url(full_url);
+                var base_url = this.get_base_url(full_url);
                 console.log(base_url);
 
                 // Store that
-                beaconid_to_data[beacon.id]['url'] = base_url;
+                this._device_to_data[beacon.id]['url'] = base_url;
 
                 // Now see if we can get parse.js
                 request(base_url + FILENAME_PARSE, function (req_parse_err, response, body) {
                     if (!req_parse_err && response.statusCode == 200) {
                         console.log('Fetching and loading ' + FILENAME_PARSE + ' for ' + full_url);
-                        beaconid_to_data[beacon.id]['parse.js'] = body;
+                        this._device_to_data[beacon.id]['parse.js'] = body;
 
                         try {
-                            var parser = require_from_string(body, base_url + FILENAME_PARSE);
-                            beaconid_to_data[beacon.id].parser = parser;
+                            var parser = this.require_from_string(body, base_url + FILENAME_PARSE);
+                            this._device_to_data[beacon.id].parser = parser;
                         } catch (e) {}
 
                     }
                 });
 
-                // And meta.json
-                request(base_url + FILENAME_META, function (meta_parse_err, response, body) {
-                    if (!meta_parse_err && response.statusCode == 200) {
-                        console.log('Fetching and parsing ' + FILENAME_META + ' for ' + full_url);
+                // // And meta.json
+                // request(base_url + FILENAME_META, function (meta_parse_err, response, body) {
+                //     if (!meta_parse_err && response.statusCode == 200) {
+                //         console.log('Fetching and parsing ' + FILENAME_META + ' for ' + full_url);
 
-                        try {
-                            var meta = JSON.parse(body);
-                            beaconid_to_data[beacon.id].meta = meta;
-                        } catch (e) {}
-                    }
-                });
+                //         try {
+                //             var meta = JSON.parse(body);
+                //             this._device_to_data[beacon.id].meta = meta;
+                //         } catch (e) {}
+                //     }
+                // });
             }
         });
 
     }
-});
+};
 
-start();
+
+// If this is true, we are running this file directly and should
+// start the gateway.
+if (require.main === module) {
+    new BleGateway().start();
+}
+
+module.exports = BleGateway;
