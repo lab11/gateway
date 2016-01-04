21496541d725db7937d79b64bd9614e5b11726a1
Brad Campbell <bradjc5@gmail.com>
2016-01-04 18:58:35 -0500

    add print example

---
+++ software/ble-gateway-examples/print.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+/*******************************************************************************
+ * Print formatted advertisement packets to the console.
+ ******************************************************************************/
+
+var BleGateway = require('BleGateway');
+var bleg = new BleGateway();
+
+console.log(bleg)
+bleg.on('advertisement', function (adv_obj) {
+	console.log(adv_obj);
+});
+
+bleg.start();
