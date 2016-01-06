2783721aa7f4db67e65d78d72bee86cfad642032
Brad Campbell <bradjc5@gmail.com>
2016-01-05 22:22:18 -0500

    clearer port

--- software/redirect-server/redirect-server.py
+++ software/redirect-server/redirect-server.py
@@ -26,6 +26,4 @@ def all(path):
 
 # Run server
 if __name__ == '__main__':
-	# Bind to PORT if defined, otherwise default to 5000.
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=5000)
