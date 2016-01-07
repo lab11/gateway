42678be267fbe58edd3cc53f8e919ea8a7b2d377
Brad Campbell <bradjc5@gmail.com>
2016-01-07 18:43:12 -0500

    move most basic to example folder

--- software/ble-gateway/examples/print.js
+++ software/ble-gateway/examples/print.js
@@ -4,10 +4,9 @@
  * Print formatted advertisement packets to the console.
  ******************************************************************************/
 
-var BleGateway = require('ble-gateway');
+var BleGateway = require('../ble-gateway');
 var bleg = new BleGateway();
 
-console.log(bleg)
 bleg.on('advertisement', function (adv_obj) {
 	console.log(adv_obj);
 });
