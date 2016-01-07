3fc868a361aa3f63b9c27a45a0497a9f39ec3770
Brad Campbell <bradjc5@gmail.com>
2016-01-07 18:14:40 -0500

    multicast recv

---
+++ software/examples/node-multicast.js
@@ -0,0 +1,18 @@
+var PORT = 5007 ;
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
+
+client.on('listening', function () {
+    var address = client.address();
+    console.log('UDP Client listening on ' + address.address + ":" + address.port);
+    client.setBroadcast(true)
+    client.setMulticastTTL(128);
+    client.addMembership('225.0.0.199');
+});
+
+client.on('message', function (message, remote) {
+    console.log('A: Epic Command Received. Preparing Relay.');
+    console.log('B: From: ' + remote.address + ':' + remote.port +' - ' + message);
+});
+
+client.bind(PORT);
