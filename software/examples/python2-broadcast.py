f92daba83512cc2d32a0b1ac547e111aacaef690
Brad Campbell <bradjc5@gmail.com>
2016-01-08 14:43:19 -0500

    moved to terraswarm org

--- software/examples/python2-broadcast.py
+++
@@ -1,9 +0,0 @@
-import json
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('255.255.255.255', 3002))
-while True:
-	message = s.recvfrom(1024)
-	adv_obj = json.loads(message[0])
-	print(adv_obj)
