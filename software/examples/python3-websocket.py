fd913bf7a61e72bdeb52eaa417451366413d0575
Brad Campbell <bradjc5@gmail.com>
2016-01-07 19:18:44 -0500

    added py3.5 websocket example

---
+++ software/examples/python3-websocket.py
@@ -0,0 +1,17 @@
+import asyncio
+import json
+
+import websockets
+
+HOST = '127.0.0.1'
+PORT = 3001
+
+async def recv():
+	async with websockets.connect('ws://{}:{}'.format(HOST, PORT)) as websocket:
+
+		while True:
+			message = await websocket.recv()
+			adv_obj = json.loads(message)
+			print(adv_obj)
+
+asyncio.get_event_loop().run_until_complete(recv())
