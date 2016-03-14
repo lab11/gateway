6c28590b0ecc1b1a62c0dc00e5836c97e1ec9dd4
Sam DeBruin <sdebruin@umich.edu>
2016-03-14 18:38:35 -0400

    stripped down mqtt-sql

--- software/gateway-mqtt-sql/gateway-mqtt-sql.js
+++ software/gateway-mqtt-sql/gateway-mqtt-sql.js
@@ -44,6 +44,10 @@ var mqtt_watchdog = new watchout(1*60*1000, function(didCancelWatchdog) {
 });
 
 // connect to MQTT broker
+var post_count = 0;
+var UPLOAD_COUNT = 1000;
+var file_start_time = 0;
+var FILE_TIMEOUT = 30;
 MQTTDiscover.on('mqttBroker', function (mqtt_client) {
     console.log("Connected to MQTT broker: " + mqtt_client.options.host);
 
@@ -61,41 +65,55 @@ MQTTDiscover.on('mqttBroker', function (mqtt_client) {
 
         // log packets in SQL format
         log_to_sql(adv);
+        if(post_count == 0) {      // Mark the start time of the first packet in this file
+            file_start_time = curr_time;
+        }
+        post_count += 1;
 		
 		// if enough packets have been logged, push to SQL
-		post_to_sql();
+        if(post_count >= UPLOAD_COUNT || (curr_time - file_start_time) >= FILE_TIMEOUT) {
+		  post_to_sql();
+        }
     });
 });
 
+// Log csv-formatted advertisements to a temp file
+function log_to_sql (adv) {
+    console.log(adv);
+}
+
 // post JSON advertisements to GATD
-var post_buffer = [];
-var last_post_time = 0;
-function post_to_gatd (adv) {
-    var curr_time = Date.now()/1000;
+// var post_buffer = [];
+// var last_post_time = 0;
+// function post_to_gatd (adv) {
+//     var curr_time = Date.now()/1000;
+
+
+
 
     // buffer several advertisements and post the entire list to GATD
-    post_buffer.push(adv);
-    if (post_buffer.length > POST_BUFFER_LEN || (curr_time-last_post_time) > POST_TIMEOUT) {
-        last_post_time = curr_time;
-        var buf = post_buffer;
-        post_buffer = [];
+    // post_buffer.push(adv);
+    // if (post_buffer.length > POST_BUFFER_LEN || (curr_time-last_post_time) > POST_TIMEOUT) {
+    //     last_post_time = curr_time;
+    //     var buf = post_buffer;
+    //     post_buffer = [];
 
-        // time to send array
-        var req = {
-            url: config.post_url,
-            method: "POST",
-            json: buf,
-        };
-        request(req, function (error, response, body) {
-            if (!(error || response.statusCode != 200)) {
-                // post successful, reset watchdog
-                gatd_watchdog.reset();
-                if (gatd_down) {
-                    console.log("Getting packets again");
-                    gatd_down = false;
-                }
-            }
-        });
-    }
+    //     // time to send array
+    //     var req = {
+    //         url: config.post_url,
+    //         method: "POST",
+    //         json: buf,
+    //     };
+    //     request(req, function (error, response, body) {
+    //         if (!(error || response.statusCode != 200)) {
+    //             // post successful, reset watchdog
+    //             gatd_watchdog.reset();
+    //             if (gatd_down) {
+    //                 console.log("Getting packets again");
+    //                 gatd_down = false;
+    //             }
+    //         }
+    //     });
+    // }
 }
 
