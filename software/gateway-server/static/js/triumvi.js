5d389f54267fb0bc9b03586dfbd2e31f8f5d410e
Brad Campbell <bradjc5@gmail.com>
2016-07-29 01:27:07 -0400

    use mqtt websockets instead of -publish

--- software/gateway-server/static/js/triumvi.js
+++ software/gateway-server/static/js/triumvi.js
@@ -2,7 +2,7 @@
 var queryDict = {}
 location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
 
-var ws_host = queryDict.ws_host || window.location.hostname + ':3001';
+var ws_host = queryDict.ws_host || window.location.hostname + ':9001';
 
 var ws = new WebSocket('ws://' + ws_host);
 ws.onopen = function() { };
