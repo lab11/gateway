9170845e96b5097eab6a330c9a5ba787d0869d37
Brad <bradjc5@gmail.com>
2016-12-11 23:58:02 -0500

    interval data working

--- software/gateway-triumvi-server/gateway-triumvi-server.js
+++ software/gateway-triumvi-server/gateway-triumvi-server.js
@@ -10,10 +10,8 @@ var express    = require('express');
 var nunjucks   = require('nunjucks');
 var bodyParser = require('body-parser');
 var sqlite3    = require('sqlite3');
-// var prettyjson = require('prettyjson');
-// var getmac     = require('getmac');
 var async      = require('async');
-// var request    = require('request');
+
 
 var app = express();
 
@@ -25,21 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 var nunjucksEnv = nunjucks.configure(__dirname + '/templates', {watch: true});
 
-// // Setup templates
-// var nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(__dirname + '/templates'));
-
-// nunjucksEnv.addFilter('is_object', function(obj) {
-//   return typeof obj === 'object';
-// });
-
-// nunjucksEnv.configure({watch: true});
 var config = {};
 config.database_file = '/home/bradjc/git/gateway/software/gateway-triumvi-sqlite/triumvi.sqlite';
-
-
-/*******************************************************************************
- * SQL
- ******************************************************************************/
 
 
 /*******************************************************************************
@@ -61,6 +46,8 @@ app.get('/', function (req, res) {
 // Show a graph of a value in real time
 app.get('/triumvi/data', function (req, res) {
 
+	// Fill in the form with valid data based on which Triumvis have been
+	// seen so far.
 	var data = {
 		triumvi_ids: [],
 		panel_ids: [],
@@ -103,9 +90,25 @@ app.get('/triumvi/data', function (req, res) {
 app.post('/triumvi/data/download', function (req, res) {
 	console.log(req.body)
 
-	var query = 'SELECT * FROM triumvi ';
+	var query = 'SELECT ';
+	var group_by = '';
 	var param = {};
 	var filename = 'triumvi_';
+	var minutes = null;
+
+	// Time resolution changes our select query
+	if (req.body.resolution == 'all') {
+		query += '* ';
+	} else {
+		// Use GROUP BY and AVG to downsample the data.
+		minutes = parseInt(req.body.resolution.substr(0, req.body.resolution.length-3));
+		query += 'ROUND(timestamp/($minutes * 60 * 1000)) AS timekey, AVG(power_watts) as POWER_WATTS ';
+		param.$minutes = minutes;
+		group_by = 'GROUP BY timekey ';
+	}
+
+	// Need table
+	query += 'FROM triumvi ';
 
 	// Filter for which triumvis
 	if (req.body.which == 'panel-circuit') {
@@ -139,6 +142,12 @@ app.post('/triumvi/data/download', function (req, res) {
 		filename += 'end_';
 	}
 
+	// Might need group by. group_by may also be just an empty string.
+	query += group_by;
+
+	// Resolution
+	filename += req.body.resolution;
+
 	// Filetype
 	filename += '.' + req.body.format;
 
@@ -151,38 +160,59 @@ app.post('/triumvi/data/download', function (req, res) {
 
 
 
+	// Headers for the CSV file.
 	var HEADERS = ['TRIUMVI_ID', 'TIMESTAMP', 'POWER_WATTS', 'POWER_FACTOR',
 	               'VOLTAGE_RMS_VOLTS', 'CURRENT_RMS_AMPS', 'PANEL_ID', 'CIRCUIT_ID'];
 
+	// Use a local file to store the resulting data, then let the user download
+	// that file.
 	var file = fs.openSync(filename, 'w');
 
 	if (req.body.format == 'csv') {
 		// Add first row
-		for (var i=0; i<HEADERS.length; i++) {
-			fs.writeSync(file, HEADERS[i]);
-			if (i == HEADERS.length-1) {
-				fs.writeSync(file, '\n');
-			} else {
-				fs.writeSync(file, ',');
+		if (req.body.resolution == 'all') {
+			for (var i=0; i<HEADERS.length; i++) {
+				fs.writeSync(file, HEADERS[i]);
+				if (i == HEADERS.length-1) {
+					fs.writeSync(file, '\n');
+				} else {
+					fs.writeSync(file, ',');
+				}
 			}
+		} else {
+			// Data was downsampled.
+			fs.writeSync(file, 'TIMESTAMP,POWER_WATTS\n');
 		}
 	}
 
+	// Setup the connection to the DB and run the query.
 	var db = new sqlite3.Database(config.database_file);
 	db.each(query, param, function (err, row) {
 
+		// Write the data to the file.
 		if (req.body.format == 'json') {
+			if (req.body.resolution != 'all') {
+				// In downsample mode we need to fix up the time key.
+				row.TIMESTAMP = row.timekey * minutes * 60 * 1000;
+				delete row.timekey;
+			}
 			fs.writeSync(file, JSON.stringify(row) + '\n');
 
 		} else if (req.body.format == 'csv') {
 			var str = '';
-			for (var i=0; i<HEADERS.length; i++) {
-				str += row[HEADERS[i]];
-				if (i == HEADERS.length-1) {
-					str += '\n';
-				} else {
-					str += ',';
+
+			if (req.body.resolution == 'all') {
+				for (var i=0; i<HEADERS.length; i++) {
+					str += row[HEADERS[i]];
+					if (i == HEADERS.length-1) {
+						str += '\n';
+					} else {
+						str += ',';
+					}
 				}
+			} else {
+				// Only timestamp and power
+				str += row.timekey * minutes * 60 * 1000 + ',' + row.POWER_WATTS + '\n';
 			}
 			fs.writeSync(file, str);
 		}
@@ -191,10 +221,6 @@ app.post('/triumvi/data/download', function (req, res) {
 		fs.closeSync(file);
 		res.download(filename);
 	});
-
-
-
-
 });
 
 /*******************************************************************************
