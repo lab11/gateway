e55e8f0480d97399fb2a280a75e3fe4533929b83
Brad <bradjc5@gmail.com>
2017-05-05 18:28:04 -0400

    get script dir

--- startup/edison-v3-cc2538/flash_cc2538.sh
+++ startup/edison-v3-cc2538/flash_cc2538.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get this dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Assert the program enable pin on the cc2538
 DIRECTORY="/sys/class/gpio/gpio45/"
 if [ ! -d "$DIRECTORY" ]; then
@@ -9,7 +12,7 @@ echo "out" > /sys/class/gpio/gpio45/direction
 echo 1 > /sys/class/gpio/gpio45/value
 
 # Use the BSL script to flash the CC2538
-python cc2538-bsl.py -b 115200 -e -w -v  cc2538.bin
+python $DIR/cc2538-bsl.py -b 115200 -e -w -v $DIR/cc2538.bin
 
 # De-assert programming pin
 echo 0 > /sys/class/gpio/gpio45/value
