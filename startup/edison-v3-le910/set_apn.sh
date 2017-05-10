f4d16cce4e1c393b8ff07ff4687f1355229d6d22
Brad <bradjc5@gmail.com>
2017-05-09 21:54:54 -0400

    also change networkmanager

--- startup/edison-v3-le910/set_apn.sh
+++ startup/edison-v3-le910/set_apn.sh
@@ -8,12 +8,14 @@ MODEL=`qmicli -d /dev/cdc-wdm0 --dms-get-model`
 
 # If the radio is a "LE910-SVG", it supports verizon
 if [[ $MODEL == *"LE910-SVG"* ]]; then
-	echo "Configuring /etc/qmi-network.conf for Verizon modem."
+	echo "Configuring /etc/qmi-network.conf and NetworkManager for Verizon modem."
 	sed -i -E "s/^APN=(.*)$/APN=VZWINTERNET/g" /etc/qmi-network.conf
+	sed -i -E "s/^apn=(.*)$/apn=VZWINTERNET/g" /etc/NetworkManager/system-connections/LE910
 fi
 
 # If the radio is a "LE910-NAG", it supports at&t
 if [[ $MODEL == *"LE910-NAG"* ]]; then
-	echo "Configuring /etc/qmi-network.conf for AT&T modem."
+	echo "Configuring /etc/qmi-network.conf and NetworkManager for AT&T modem."
 	sed -i -E "s/^APN=(.*)$/APN=I2GOLD/g" /etc/qmi-network.conf
+	sed -i -E "s/^apn=(.*)$/apn=I2GOLD/g" /etc/NetworkManager/system-connections/LE910
 fi
