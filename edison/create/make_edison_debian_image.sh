68eaba30c1015404835ecf49d506cbe334c911f9
Brad <bradjc5@gmail.com>
2017-05-19 01:28:06 -0400

    add sensu to magic script

--- edison/create/make_edison_debian_image.sh
+++ edison/create/make_edison_debian_image.sh
@@ -272,6 +272,17 @@ fi
 
 # Umich specific services
 if [[ $UMICH -eq 1 ]]; then
+	# Install sensu
+	$CHROOTCMD wget -q http://repositories.sensuapp.org/apt/pubkey.gpg -O- | $CHROOTCMD apt-key add -
+	echo "deb     http://repositories.sensuapp.org/apt sensu main" > $ROOTDIR/etc/apt/sources.list.d/sensu.list
+	$CHROOTCMD apt update
+	$CHROOTCMD apt -y install sensu
+	rm $ROOTDIR/etc/init.d/sensu*
+	wget https://raw.githubusercontent.com/sensu/sensu-build/master/sensu_configs/systemd/sensu-client.service $ROOTDIR/etc/systemd/system/
+	ln -s ../sensu-client.service $ROOTDIR/etc/systemd/system/multi-user.target.wants/
+	cp -r gateway-private/sensu $ROOTDIR/etc/
+	$CHROOTCMD npm --prefix /home/debian/gateway-tools/gateway install --build-from-source
+
 	# Make sure we have the config information for internal gateways
 	sudo -u $USER git submodule update --init gateway-private
 	cp -r gateway-private/swarm-gateway $ROOTDIR/etc/
