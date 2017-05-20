18ea044a3cfa2f8dd636b98a882fedef07703f4f
Brad <bradjc5@gmail.com>
2017-05-20 01:33:09 -0400

    adv-gateway-ip: make sure we have an address

--- software/adv-gateway-ip/adv-gateway-ip.js
+++ software/adv-gateway-ip/adv-gateway-ip.js
@@ -11,6 +11,19 @@ var ble_adv_options = {
     name: os.hostname(),
 };
 
+// Whether or not we found an IP address to advertise
+var advertising = false;
+
+// Check in 1 minute to see if we started advertising.
+// Otherwise exit so we restart to see if we have an IP then.
+setTimeout(function () {
+    if (advertising == false) {
+        console.log('Could not find IP address. Exiting.');
+        process.exit(-1);
+    }
+}, 1000*60);
+
+// Main code for finding and advertising the correct IP address.
 var ifaces  = os.networkInterfaces();
 Object.keys(ifaces).forEach(function (ifname) {
     ifaces[ifname].forEach(function (iface) {
@@ -22,6 +35,13 @@ Object.keys(ifaces).forEach(function (ifname) {
         // we have found the first non-local IPv4 address. Advertise it
         console.log('Advertising http://' + iface.address + ' with device name "' + ble_adv_options.name +'"');
         eddystoneBeacon.advertiseUrl('http://' + iface.address, ble_adv_options);
+        advertising = true;
     });
 });
 
+// Set a long timeout to cause systemd to restart this process. This gives us
+// a chance to update the advertisement in case the IP address changes.
+setTimeout(function () {
+    console.log('Exiting to restart.');
+    process.exit(0);
+}, 1000*60*60*2);
