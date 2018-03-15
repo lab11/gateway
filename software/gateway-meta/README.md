f7f1a9602b4ecb34c3e930a7936c04f5da066df3
Brad Campbell <bradjc5@gmail.com>
2018-03-14 23:22:09 -0400

    add gateway meta publisher
    super easy way to track IP addresses

---
+++ software/gateway-meta/README.md
@@ -0,0 +1,20 @@
+Gateway Meta
+============
+
+Publishes metadata about the gateway as a data stream.
+
+Example Packet
+--------------
+
+```
+var adv_obj = {
+    device: "GatewayAddresses",
+    addresses: <comma separated string of ip addresses>,
+
+    _meta = {
+        received_time: new Date().toISOString(),
+        device_id:     gateway_id_no_colons,
+        receiver:      'gateway-meta',
+        gateway_id:    gateway_id
+	}
+};
---
+++ software/gateway-meta/gateway-meta.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+var async = require('async');
+var mqtt  = require('mqtt');
+
+var gatewayId = require('lab11-gateway-id');
+
+
+var MQTT_TOPIC_NAME = 'gateway-data';
+
+var gateway_id = '';
+
+
+// Get IP address
+function get_ip_addresses (cb) {
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+
+    var local_ip_addresses = [];
+    async.eachSeries(Object.keys(ifaces), function (ifname, done) {
+        if (ifname != 'lo') {
+            async.forEachOfSeries(ifaces[ifname], function (iface, index, done2) {
+                local_ip_addresses.push({ifname: ifname, index: index, address: iface.address});
+                done2();
+            }, function (err) {
+                done();
+            });
+        } else {
+            done();
+        }
+    }, function (err) {
+        cb(local_ip_addresses);
+    });
+}
+
+
+// Need to get the gateway ID to publish in the data packets.
+gatewayId.id((id) => {
+    gateway_id = id;
+});
+
+var mqtt_client = mqtt.connect('mqtt://localhost');
+mqtt_client.on('connect', function () {
+
+    // Publish IP addresses
+    function publish () {
+        get_ip_addresses(function (addresses) {
+            var addr_str = addresses.map(el => el.address).join(', ');
+
+            var adv_obj = {
+                device: "GatewayAddresses",
+                addresses: addr_str,
+            };
+
+            adv_obj._meta = {
+                received_time: new Date().toISOString(),
+                device_id:     gateway_id.replace(/:/g, ''),
+                receiver:      'gateway-meta',
+                gateway_id:    gateway_id
+            };
+
+            mqtt_client.publish(MQTT_TOPIC_NAME, JSON.stringify(adv_obj));
+        });
+    }
+
+    // Do one at the start
+    publish();
+
+    // Every so often check for ip address changes.
+    setInterval(function () {
+        publish();
+    }, 1000 * 60 * 5);
+
+});
